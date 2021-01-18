e973ec4315771e084e14bd98caf43c0e93c8dcec
ghemsley <ghemsley@protonmail.ch>
2021-01-18 16:32:08 -0600

    commit tree f521c3413fedc21866667bc5d37b0e4ea7523218

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,9 @@
-require_relative "../config/environment.rb"
+require_relative '../config/environment.rb'
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  column_names.each do |column_name|
+    attr_accessor column_name.to_sym
+  end
 end
